1ced3d0626d6806d0380ec8f54d7a0329aab3bfe
liuyi <liuyi@ushow.media>
2017-02-10 02:31:40 +0800

    补充数据，后台库和api管理库是分开称两个文件

--- database/lumen-api.sql
+++ database/lumen-api.sql
@@ -1,27 +1,29 @@
--- phpMyAdmin SQL Dump
--- version 4.6.5.2
--- https://www.phpmyadmin.net/
+-- MySQL dump 10.16  Distrib 10.1.17-MariaDB, for osx10.12 (x86_64)
 --
--- Host: 127.0.0.1
--- Generation Time: 2016-12-21 12:39:51
--- 服务器版本： 10.1.17-MariaDB
--- PHP Version: 7.1.0
+-- Host: localhost    Database: lumen-api
+-- ------------------------------------------------------
+-- Server version 10.1.17-MariaDB
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Database: `lumen-api`
+-- Table structure for table `channel`
 --
 
--- --------------------------------------------------------
-
---
--- 表的结构 `channel`
---
-
+DROP TABLE IF EXISTS `channel`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `channel` (
-  `id` int(11) NOT NULL COMMENT '频道 id',
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '频道 id',
   `name` varchar(100) NOT NULL COMMENT '频道名称',
   `region` enum('whole','region','section','group') NOT NULL COMMENT '地域类型，全国，地区，地段，群',
   `banner` text NOT NULL COMMENT 'banner 条',
@@ -29,48 +31,29 @@ CREATE TABLE `channel` (
   `updated_at` int(10) NOT NULL,
   `deleted_at` int(10) DEFAULT NULL,
   `style` text NOT NULL COMMENT '样式',
-  `body` text NOT NULL COMMENT '正文内容'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `body` text NOT NULL COMMENT '正文内容',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- 转存表中的数据 `channel`
+-- Dumping data for table `channel`
 --
 
-INSERT INTO `channel` (`id`, `name`, `region`, `banner`, `created_at`, `updated_at`, `deleted_at`, `style`, `body`) VALUES
-(1, '北京', 'region', '', 1434343434, 1481988347, NULL, '11,12', ''),
-(2, '美女', 'whole', '', 1434343434, 1481988345, NULL, '', ''),
-(3, '西直门', 'section', '', 1434343434, 1481988344, NULL, '', '');
-
--- --------------------------------------------------------
-
---
--- 表的结构 `news`
---
-
-CREATE TABLE `news` (
-  `id` int(11) NOT NULL,
-  `title` varchar(50) NOT NULL,
-  `content` text NOT NULL,
-  `author` varchar(50) NOT NULL,
-  `created_at` int(10) NOT NULL,
-  `updated_at` int(10) NOT NULL,
-  `deleted_at` int(10) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+LOCK TABLES `channel` WRITE;
+/*!40000 ALTER TABLE `channel` DISABLE KEYS */;
+INSERT INTO `channel` VALUES (1,'北京','region','',1434343434,1481988347,NULL,'11,12',''),(2,'美女','whole','',1434343434,1481988345,NULL,'',''),(3,'西直门','section','',1434343434,1481988344,NULL,'','');
+/*!40000 ALTER TABLE `channel` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- 转存表中的数据 `news`
+-- Table structure for table `channel-template`
 --
 
-INSERT INTO `news` (`id`, `title`, `content`, `author`, `created_at`, `updated_at`, `deleted_at`) VALUES
-(1, 'title', 'content', 'author', 1434343434, 1481988350, NULL);
-
--- --------------------------------------------------------
-
---
--- 表的结构 `tile-style`
---
-
-CREATE TABLE `tile-style` (
+DROP TABLE IF EXISTS `channel-template`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `channel-template` (
   `id` int(11) NOT NULL COMMENT 'ID',
   `title` varchar(100) NOT NULL COMMENT '标题',
   `brief` varchar(100) NOT NULL COMMENT '简介',
@@ -79,22 +62,55 @@ CREATE TABLE `tile-style` (
   `type` enum('marquee','picture','title','icon') NOT NULL COMMENT '类型: 图片  标题',
   `picture-type` enum('icon','big','normal','landscape','portrait','square') NOT NULL COMMENT '图片类型：',
   `line-take` enum('1','2','3','4','5') NOT NULL COMMENT '一行多少个'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- 转存表中的数据 `tile-style`
+-- Dumping data for table `channel-template`
 --
 
-INSERT INTO `tile-style` (`id`, `title`, `brief`, `sketch`, `introduction`, `type`, `picture-type`, `line-take`) VALUES
-(11, '', '', '', '', 'marquee', 'big', ''),
-(12, '', '', '', '', 'picture', 'portrait', '2');
-
--- --------------------------------------------------------
+LOCK TABLES `channel-template` WRITE;
+/*!40000 ALTER TABLE `channel-template` DISABLE KEYS */;
+INSERT INTO `channel-template` VALUES (11,'','','','','marquee','big',''),(12,'','','','','picture','portrait','2');
+/*!40000 ALTER TABLE `channel-template` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- 表的结构 `user`
+-- Table structure for table `news`
 --
 
+DROP TABLE IF EXISTS `news`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `news` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(50) NOT NULL,
+  `content` text NOT NULL,
+  `author` varchar(50) NOT NULL,
+  `created_at` int(10) NOT NULL,
+  `updated_at` int(10) NOT NULL,
+  `deleted_at` int(10) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `news`
+--
+
+LOCK TABLES `news` WRITE;
+/*!40000 ALTER TABLE `news` DISABLE KEYS */;
+INSERT INTO `news` VALUES (1,'title','content','author',1434343434,1481988350,NULL);
+/*!40000 ALTER TABLE `news` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user` (
   `id` int(16) NOT NULL COMMENT '用户 ID',
   `account` varchar(20) NOT NULL COMMENT '账号',
@@ -104,51 +120,29 @@ CREATE TABLE `user` (
   `deleted_at` int(10) DEFAULT NULL COMMENT '软删除时间',
   `created_at` int(10) NOT NULL COMMENT '创建时间',
   `updated_at` int(10) NOT NULL COMMENT '上次登录时间',
-  `channels_id` text NOT NULL COMMENT '用户的频道'
+  `channels_id` text NOT NULL COMMENT '用户的频道',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `account` (`account`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- 转存表中的数据 `user`
+-- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `account`, `password`, `token`, `token_secret`, `deleted_at`, `created_at`, `updated_at`, `channels_id`) VALUES
-(1, 'koocyton@gmail.com', '6e5e32f49d30db450234d6c3ec2625d5', '89e838b6723a9c6ece25', '837d578dfbe3c8c9539ab65d669c42df9046', NULL, 1234567890, 1482313611, '1,2,3'),
-(2, 'doopps@gmail.com', '111', '123', '123', NULL, 1434343434, 1481988357, '1,2,3');
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'koocyton@gmail.com','6e5e32f49d30db450234d6c3ec2625d5','89e838b6723a9c6ece25','837d578dfbe3c8c9539ab65d669c42df9046',NULL,1234567890,1483699392,'1,2,3'),(2,'doopps@gmail.com','111','123','123',NULL,1434343434,1481988357,'1,2,3');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Indexes for dumped tables
---
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
---
--- Indexes for table `channel`
---
-ALTER TABLE `channel`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `news`
---
-ALTER TABLE `news`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `account` (`account`);
-
---
--- 在导出的表使用AUTO_INCREMENT
---
-
---
--- 使用表AUTO_INCREMENT `channel`
---
-ALTER TABLE `channel`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '频道 id', AUTO_INCREMENT=4;
---
--- 使用表AUTO_INCREMENT `news`
---
-ALTER TABLE `news`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+-- Dump completed on 2017-02-10  2:30:29
--- database/lumen-backend.sql
+++ database/lumen-backend.sql
@@ -1,113 +1,121 @@
--- phpMyAdmin SQL Dump
--- version 4.6.5.2
--- https://www.phpmyadmin.net/
+-- MySQL dump 10.16  Distrib 10.1.17-MariaDB, for osx10.12 (x86_64)
 --
--- Host: 127.0.0.1
--- Generation Time: 2016-12-21 12:41:13
--- 服务器版本： 10.1.17-MariaDB
--- PHP Version: 7.1.0
+-- Host: localhost    Database: lumen-backend
+-- ------------------------------------------------------
+-- Server version 10.1.17-MariaDB
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Database: `lumen-backend`
+-- Table structure for table `manager`
 --
 
--- --------------------------------------------------------
-
---
--- 表的结构 `manager`
---
-
+DROP TABLE IF EXISTS `manager`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `manager` (
-  `id` int(16) NOT NULL COMMENT '管理员 id',
+  `id` int(16) NOT NULL AUTO_INCREMENT COMMENT '管理员 id',
   `account` varchar(20) NOT NULL COMMENT '账号',
+  `username` varchar(254) NOT NULL,
   `password` char(32) NOT NULL COMMENT '密码',
+  `token` varchar(50) NOT NULL,
+  `token_secret` varchar(50) NOT NULL,
   `deleted_at` int(10) DEFAULT NULL COMMENT '软删除的时间戳',
   `created_at` int(10) NOT NULL COMMENT '创建时间',
   `updated_at` int(10) NOT NULL COMMENT '更新时间',
-  `privileges` text NOT NULL COMMENT '权限'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `privileges` text NOT NULL COMMENT '权限',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `account` (`account`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- 转存表中的数据 `manager`
+-- Dumping data for table `manager`
 --
 
-INSERT INTO `manager` (`id`, `account`, `password`, `deleted_at`, `created_at`, `updated_at`, `privileges`) VALUES
-(1, 'koocyton@gmail.com', '6e5e32f49d30db450234d6c3ec2625d5', 1482317140, 1434343434, 1482313434, '');
-
--- --------------------------------------------------------
+LOCK TABLES `manager` WRITE;
+/*!40000 ALTER TABLE `manager` DISABLE KEYS */;
+INSERT INTO `manager` VALUES (1,'koocyton@gmail.com','刘毅','6e5e32f49d30db450234d6c3ec2625d5','1e1517365bdd4dcaafa0','dcb382ae91dfba0c8382c438766d4ccf96db',NULL,1434343434,1486664537,''),(2,'liuyi.hn@gmail.com','刘毅','6e5e32f49d30db450234d6c3ec2625d5','','',NULL,1434343434,1484410152,''),(3,'doopps@gmail.com','刘毅','6e5e32f49d30db450234d6c3ec2625d5','','',NULL,1434343434,1484373000,'');
+/*!40000 ALTER TABLE `manager` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- 表的结构 `operation-record`
+-- Table structure for table `operation-record`
 --
 
+DROP TABLE IF EXISTS `operation-record`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `operation-record` (
-  `id` int(11) NOT NULL COMMENT '日志顺序id',
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '日志顺序id',
   `manager_id` int(11) NOT NULL COMMENT '操作员 id',
   `manager_name` varchar(50) NOT NULL COMMENT '操作者名称',
   `request_method` enum('GET','POST') NOT NULL COMMENT 'request method',
   `request_uri` varchar(100) NOT NULL COMMENT '请求的 url',
   `post_data` text NOT NULL COMMENT 'post 数据',
-  `created_at` int(10) NOT NULL COMMENT '请求的时间'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
-
--- --------------------------------------------------------
+  `created_at` int(10) NOT NULL COMMENT '请求的时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- 表的结构 `task`
+-- Dumping data for table `operation-record`
 --
 
+LOCK TABLES `operation-record` WRITE;
+/*!40000 ALTER TABLE `operation-record` DISABLE KEYS */;
+INSERT INTO `operation-record` VALUES (1,1,'koocyton@gmail.com','GET','operation/list','',1482576183),(2,1,'koocyton@gmail.com','GET','operation/list','',1482576192),(3,1,'koocyton@gmail.com','GET','operation/list','',1482576196),(4,1,'koocyton@gmail.com','GET','operation/list','',1482576227),(5,1,'koocyton@gmail.com','GET','login/signout','',1482576231),(6,1,'koocyton@gmail.com','GET','login','',1482576231),(7,1,'koocyton@gmail.com','POST','login/signin','{\"_token\":\"\",\"account\":\"koocyton@gmail.com\",\"password\":\"qe2ao3ba\"}',1482576232),(8,1,'koocyton@gmail.com','GET','manager/list','',1482576233),(9,1,'koocyton@gmail.com','GET','operation/list','',1482576235);
+/*!40000 ALTER TABLE `operation-record` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `task`
+--
+
+DROP TABLE IF EXISTS `task`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `task` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(200) NOT NULL,
-  `author` int(11) NOT NULL,
+  `category` enum('task','bug','doc') NOT NULL,
+  `author` varchar(255) NOT NULL,
+  `owner` text NOT NULL,
+  `subscribers` text NOT NULL,
+  `description` text NOT NULL,
   `status` int(1) NOT NULL,
   `created_at` int(10) NOT NULL,
   `updated_at` int(10) NOT NULL,
-  `deleted_at` int(10) NOT NULL
+  `deleted_at` int(10) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Indexes for dumped tables
+-- Dumping data for table `task`
 --
 
---
--- Indexes for table `manager`
---
-ALTER TABLE `manager`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `account` (`account`);
+LOCK TABLES `task` WRITE;
+/*!40000 ALTER TABLE `task` DISABLE KEYS */;
+/*!40000 ALTER TABLE `task` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Indexes for table `operation-record`
---
-ALTER TABLE `operation-record`
-  ADD PRIMARY KEY (`id`);
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
---
--- Indexes for table `task`
---
-ALTER TABLE `task`
-  ADD PRIMARY KEY (`id`);
-
---
--- 在导出的表使用AUTO_INCREMENT
---
-
---
--- 使用表AUTO_INCREMENT `manager`
---
-ALTER TABLE `manager`
-  MODIFY `id` int(16) NOT NULL AUTO_INCREMENT COMMENT '管理员 id', AUTO_INCREMENT=2;
---
--- 使用表AUTO_INCREMENT `operation-record`
---
-ALTER TABLE `operation-record`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '日志顺序id';
---
--- 使用表AUTO_INCREMENT `task`
---
-ALTER TABLE `task`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+-- Dump completed on 2017-02-10  2:28:24

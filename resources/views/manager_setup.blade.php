488fdcf81dddf53bd780615e2af32ce6f64d3ef0
liuyi <liuyi@ushow.media>
2017-02-20 00:11:33 +0800

    加入 video-js-4.5.1.swf

--- resources/views/manager_setup.blade.php
+++ resources/views/manager_setup.blade.php
@@ -1,16 +1,15 @@
     <br>
 
-  <link href="/video-js/video-js.min.css" rel="stylesheet">
-  <style>
-  body{margin:0;}
-  .my-video{width:100%;height:480px;}
-  .my-video>.vjs-big-play-button{top:45%;left:42%;width:16%;height:70px;line-height:70px;}
-  </style>
-  <script src="/video-js/video.min.js"></script>
-  <script src="/video-js/videojs-contrib-hls.min.js"></script>
-  <script>
-  videojs.options.flash.swf = "/video-js/video-js.swf";
-  </script>
+    <link href="/video-js/video-js.min.css" rel="stylesheet">
+    <style>
+    .my-video{width:100%;height:480px;}
+    .my-video>.vjs-big-play-button{top:45%;left:42%;width:16%;height:70px;line-height:70px;}
+    </style>
+    <script src="/video-js/video.min.js"></script>
+    <script src="/video-js/videojs-contrib-hls.min.js"></script>
+    <script>
+    videojs.options.flash.swf = "/video-js/video-js-4.5.1.swf";
+    </script>
 
     <div class="container">
       <!-- Static navbar -->
@@ -25,19 +24,23 @@
           </div><!--/.nav-collapse -->
         </div><!--/.container-fluid -->
       </nav>
-     </div>
+    </div>
 
     <div class="container">
 
-      <video poster="http://vjs.zencdn.net/v/oceans.png" class="my-video video-js vjs-default-skin" controls preload="auto" autoplay="true" data-setup='{}'>
-      <source src="http://vjs.zencdn.net/v/oceans.webm" type="video/webm"></source>
-      <!-- 
-      <source src="http://vjs.zencdn.net/v/oceans.flv" type="video/x-flv"></source>
-      <source src="http://vjs.zencdn.net/v/oceans.mp4" type="video/mp4"></source>
-      <source src="http://vjs.zencdn.net/v/oceans.webm" type="video/webm"></source>
-      <source src="http://vjs.zencdn.net/v/oceans.ogv" type="video/ogg">
-      <source src="https://starmakerapp-hrd.appspot.com/api/v16/web/recordings/4664162789097472/masterhls" type="application/x-mpegURL">
-      //-->
+      <video poster="http://vjs.zencdn.net/v/oceans.png" id="my-video" class="my-video video-js vjs-default-skin" controls autoplay="true" preload="auto" data-setup='{"techOrder": ["flash"]}'>
+        <!-- 
+          data-setup='{"techOrder": ["flash"]}'
+        //-->
+        <source src="http://streambox.fr/playlists/issue_010/list.m3u8" type="video/mp4" />
+        <!-- 
+        <source src="http://vjs.zencdn.net/v/oceans.flv" type="video/x-flv" />
+        <source src="http://vjs.zencdn.net/v/oceans.mp4" type="video/mp4" />
+        <source src="http://vjs.zencdn.net/v/oceans.webm" type="video/webm" />
+        <source src="http://vjs.zencdn.net/v/oceans.ogv" type="video/ogg" />
+        <source src="https://starmakerapp-hrd.appspot.com/api/v16/web/recordings/4664162789097472/masterhls" type="application/x-mpegURL" />
+        <source src="http://streambox.fr/playlists/issue_006/sample.m3u8" type="application/x-mpegURL" />
+        //-->
       </video>
 
     </div>

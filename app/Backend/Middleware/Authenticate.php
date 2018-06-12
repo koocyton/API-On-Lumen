8c5db62cdbffe1d3fe29b92bbfa93a2353548296
koocyton <koocyton@gmail.com>
2018-06-12 21:47:20 +0800

    页面跳转逻辑处理

--- app/Backend/Middleware/Authenticate.php
+++ app/Backend/Middleware/Authenticate.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Backend\Middleware;
 
 use Closure;
@@ -6,38 +7,48 @@ use Illuminate\Contracts\Auth\Factory as Auth;
 
 class Authenticate
 {
-    /**
-     * The authentication guard factory instance.
-     *
-     * @var \Illuminate\Contracts\Auth\Factory
-     */
-    protected $auth;
+  /**
+   * The authentication guard factory instance.
+   *
+   * @var \Illuminate\Contracts\Auth\Factory
+   */
+  protected $auth;
 
-    /**
-     * Create a new middleware instance.
-     *
-     * @param  \Illuminate\Contracts\Auth\Factory  $auth
-     * @return void
-     */
-    public function __construct(Auth $auth)
-    {
-        $this->auth = $auth;
-    }
+  /**
+   * Create a new middleware instance.
+   *
+   * @param  \Illuminate\Contracts\Auth\Factory $auth
+   */
+  public function __construct(Auth $auth)
+  {
+    $this->auth = $auth;
+  }
 
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @param  string|null  $guard
-     * @return mixed
-     */
-    public function handle($request, Closure $next, $guard = null)
-    {
-        if ($this->auth->guard($guard)->guest()) {
-            // return response('Unauthorized.', 401);
-            return redirect("/login");
-        }
-        return $next($request);
+  /**
+   * Handle an incoming request.
+   *
+   * @param  \Illuminate\Http\Request $request
+   * @param  \Closure $next
+   * @param  string|null $guard
+   * @return mixed
+   */
+  public function handle($request, Closure $next, $guard = null)
+  {
+    // 未登录
+    if ($this->auth->guard($guard)->guest()) {
+      // return response('Unauthorized.', 401);
+      // ajax 请求的跳转
+      if ($request->ajax()) {
+        return response()->json(['action' => 'redirect', 'url' => '/login']);
+      }
+      // 正常的跳转
+      return redirect("/login");
     }
+    // Ajax ，显示页面
+    if ($request->ajax()) {
+      return $next($request);
+    }
+    // 不是 ajax ，显示 portal
+    return response(view("backend/portal"));
+  }
 }
